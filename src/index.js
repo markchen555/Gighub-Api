29427675f931d70c4161b9b3b6cf37d226934620
Daniel Chong <danchong625@gmail.com>
2018-03-07 14:23:20 -0800

    [ops] implements cors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import db from './db';
 import router from './routes';
 import parser from 'body-parser';
+import cors from 'cors';
 
 const PORT = process.env.PORT
 const app = express();
 
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
+app.use(cors());
 
 app.use('/api', router);
 
