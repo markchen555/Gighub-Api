f924020c4074060258b28123e4eb2f6b4a3046ff
Timothy Yoon <tim.keun.yoon@gmail.com>
2018-03-07 16:26:31 -0800

    [bug] Fixes rebase issue in package.json.

--- src/db/models/Recruiter.js
+++ src/db/models/Recruiter.js
@@ -1,12 +1,32 @@
 import db from '../db';
 import Sequelize from 'sequelize';
-import User from './User';
 import Company from './Company';
 
 const Recruiter = db.define('Recruiter', {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  photoLink: Sequelize.STRING
 })
 
+Company.hasMany(Recruiter);
 Recruiter.belongsTo(Company);
-Recruiter.belongsTo(User);
 
 export default Recruiter;
